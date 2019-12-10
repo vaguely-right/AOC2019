b2445be4dd4610e63e117d6239e26877db5c2655
Steve Lyster <steven.lyster@gmail.com>
2019-12-10 12:12:41 -0700

    Update day10.py

--- day10.py
+++ day10.py
@@ -94,6 +94,47 @@ acoords = coords200[np.argmin(dist200)]
 answer = 100 * acoords[0] + acoords[1]
 print(answer)
 
+#%%
+#Plotting, just for fun
+import matplotlib.pyplot as plt
+plt.scatter([i[0] for i in asteroids],[i[1] for i in asteroids])
+plt.gca().invert_yaxis()
+
+#%%
+#Bring Part 2 into Pandas to do some stuff
+fname = 'day10input.txt'
+with open(fname,'r') as f:
+    day10input = f.read().split(sep='\n')
+f.close()
+ny = len(day10input)-1
+nx = len(day10input[0])
+ax = []
+ay = []
+for y in range(ny):
+    for x in range(nx):
+        if day10input[y][x] == '#':
+            ax.append(float(x))
+            ay.append(float(y))
+
+maxc = [20.0,21.0]
+maxa = 292
+asteroids = pd.DataFrame({'x':ax,'y':ay})
+asteroids['dist'] = np.sqrt(( (asteroids.x-maxc[0])**2 + (asteroids.y-maxc[1])**2 ))
+asteroids['angle'] = np.arctan2(asteroids.x-maxc[0],maxc[1]-asteroids.y)
+asteroids.angle[asteroids.angle<0] = 2.0*np.pi+asteroids.angle
+#asteroids['vaporized'] = [np.argwhere(np.sort(asteroids.angle.unique())==i)[0][0] for i in asteroids.angle]
+
+
+vap = asteroids.copy().drop(columns=['x','y']).groupby('angle').min().reset_index()
+vap['vaporized'] = [np.argwhere(np.sort(vap.angle.unique())==i)[0][0] for i in vap.angle]
+asteroids = pd.merge(asteroids,vap.groupby('angle').min(),how='left',on=['angle','dist'])
+asteroids.vaporized[asteroids.vaporized.isna()] = 248
+
+asteroids.plot.scatter('x','y',c='vaporized',colormap='rainbow')
+plt.scatter(maxc[0],maxc[1],marker='o',s=200)
+plt.gca().invert_yaxis()
+
+
 
 
 
