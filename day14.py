d56defa47ffef4246d12e67f012beadad19ce564
Steve Lyster <steven.lyster@gmail.com>
2019-12-16 16:01:17 -0700

    Day 16

--- day14.py
+++ day14.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 #%%
 fname = 'day14example1.txt'
@@ -13,8 +14,13 @@ chemicals = [i[1].split(sep=' ')[-1] for i in d14]
 #Add ORE to make the unique elements
 elements = chemicals + ['ORE']
 #Build a dataframe for the reactions
-reactions = pd.DataFrame(columns=[elements+['QTY']],index=chemicals)
+reactions = pd.DataFrame(columns=[elements+['QTY']],index=elements)
+#reactions.at['ORE','ORE'] = 1
+reactions.at['ORE','QTY'] = 1
 for i in d14:
     prod = i[1].strip().split()[1]
     reactions.at[prod,'QTY'] = i[1].strip().split(sep=' ')[0]
-    
+    for j in range(1,len(i[0].strip().split()),2):
+        chem = i[0].strip().replace(',',' ').split()[j]
+        reactions.at[prod,chem] = i[0].strip().replace(',',' ').split()[j-1]
+reactions.loc[['FUEL']].T.dropna()
---
+++ day16.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+#%%
+#Example
+insignal = np.fromiter([i for i in '12345678'],dtype=int)
+pattern = np.array([0,1,0,-1])
+nphases = 4
+#Build the matrix to modify the input signal
+fft = np.zeros((len(insignal),len(insignal)))
+for i in range(len(insignal)):
+    fft[i] = np.tile(np.repeat(pattern,i+1),len(insignal)//(len(pattern)*(i+1))+1)[1:len(insignal)+1]
+
+signal = insignal
+for phase in range(nphases):
+    signal = abs(np.matmul(fft,signal))%10
+
+#%%
+#Example
+insignal = np.fromiter([i for i in '80871224585914546619083218645595'],dtype=int)
+pattern = np.array([0,1,0,-1])
+nphases = 100
+#Build the matrix to modify the input signal
+fft = np.zeros((len(insignal),len(insignal)))
+for i in range(len(insignal)):
+    fft[i] = np.tile(np.repeat(pattern,i+1),len(insignal)//(len(pattern)*(i+1))+1)[1:len(insignal)+1]
+
+signal = insignal
+for phase in range(nphases):
+    signal = abs(np.matmul(fft,signal))%10
+
+print(signal[0:8])
+#%%
+#Part 1
+fname = 'day16input.txt'
+with open(fname) as f:
+    day16input = f.read()[:-1]
+insignal = np.fromiter([i for i in day16input],dtype=int)
+pattern = np.array([0,1,0,-1])
+nphases = 100
+#Build the matrix to modify the input signal
+fft = np.zeros((len(insignal),len(insignal)))
+for i in range(len(insignal)):
+    fft[i] = np.tile(np.repeat(pattern,i+1),len(insignal)//(len(pattern)*(i+1))+1)[1:len(insignal)+1]
+
+signal = insignal
+for phase in range(nphases):
+    signal = abs(np.matmul(fft,signal))%10
+
+print(signal[0:8])
+
+#%%
+#Part 2 example: Trying brute force
+insignal = np.fromiter([i for i in '03036732577212944063491565474664'],dtype=int)
+signal = np.tile(insignal,10000)
+offset = int('03036732577212944063491565474664'[0:7])
+pattern = np.array([0,1,0,-1])
+nphases = 100
+#Can't do it with a matrix; Memory error.
+#fft = np.zeros((len(signal),len(signal)))
+#for i in range(len(signal)):
+#    fft[i] = np.tile(np.repeat(pattern,i+1),len(signal)//(len(pattern)*(i+1))+1)[1:len(signal)+1]
+
+for phase in range(nphases):
+    print('Phase ',phase)
+    for i in range(len(signal)): 
+        fft = np.tile(np.repeat(pattern,i+1),len(signal)//(len(pattern)*(i+1))+1)[1:len(signal)+1]
+        signal[i] = abs(np.dot(fft,signal))%10
+#    signal = abs(np.matmul(fft,signal))%10
+
+print(signal[offset:offset+8])
+
+
+#%%
+#Part 2
+#Try brute force? Doubt it.
+fname = 'day16input.txt'
+with open(fname) as f:
+    day16input = f.read()[:-1]
+insignal = np.fromiter([i for i in day16input],dtype=int)
+signal = np.tile(insignal,10000)
+offset = int(day16input[0:7])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
